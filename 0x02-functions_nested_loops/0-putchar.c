a2e21a53b47ead846b6bf3528c63a444ed98e1dd
Linvmboya <linvmboya@gmail.com>
2023-06-21 05:36:24 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
 #include "main.h"
-#include "unistd.h"
-
 /**
- * _putchar - Write a program that prints a character followed by a line
- * &c: A charater to be printed
- * Return: 0
+ *main -A program that prints the character _putchar.c
+ * Returns: 0
  */
-int _putchar(char)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
 void print_alphabet(void);
 int _putchar(char);
@@ -14,3 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
+#endif
