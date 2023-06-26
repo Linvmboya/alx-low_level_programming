b02555cd06f0a9462901cc61be246b716d203981
Linvmboya <linvmboya@gmail.com>
2023-06-26 08:16:00 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to a digit and updates its value to 98
+ * @n: the digit to input
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
