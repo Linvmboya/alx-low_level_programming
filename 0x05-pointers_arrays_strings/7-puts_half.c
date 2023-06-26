3873d1e0b765a3fd326c69ece338742ff90b7274
Linvmboya <linvmboya@gmail.com>
2023-06-26 09:51:11 -0700

    Arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * puts_half - is a function tha prints half of a string
+ * @str: the string to be printed
+ * Return: print tha last n characters of a string
+ */
+void puts_half(char *str)
+{
+
