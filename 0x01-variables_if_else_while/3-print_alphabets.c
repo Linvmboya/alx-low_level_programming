b07306f3f5d83de19b07a704ee0fa47055bfc184
Linvmboya <linvmboya@gmail.com>
2023-06-27 04:21:54 -0700

    alphABET mandatory

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,12 @@
  */
 int main(void)
 {
-	char ch(52) = "abcdefghfghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		int i;
+	char ch;
 
-	for (i = 0; i < 0; i++)
-	{
-		putchar(ch[i]);
-	}
-	putchar("\n");
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
 	return (0);
 }
