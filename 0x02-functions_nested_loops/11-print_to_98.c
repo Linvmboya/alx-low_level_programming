82b487fd991a0c8a159c71e19af6bd698866d9b5
Linvmboya <linvmboya@gmail.com>
2023-10-31 13:48:32 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,26 +8,16 @@
  */
 void print_to_98(int n)
 {
-	int a, b;
-
-	if (n <= 98)
+	if (n >= 98)
 	{
-		for (a = n; a <= 98; a++)
-		{
-			if (a != 98)
-				printf("%d, ", a);
-			else if (a == 98)
-				printf("%d \n", a);
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	else if (n >= 98)
+	else
 	{
-		for (b = n; b >= 98; b--)
-		{
-			if (b != 98)
-				printf("%d, ", b);
-			else if (b == 98)
-				printf("%d \n", b);
-		}
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
