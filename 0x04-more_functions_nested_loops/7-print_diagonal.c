acea4d526f98ccfd63793835e2df057f4a04a718
Linvmboya <linvmboya@gmail.com>
2023-11-02 13:10:14 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,8 +13,10 @@ void print_diagonal(int n)
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < n; j++)
+		{
 			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\\');
-	_putchar('\n');
 }
